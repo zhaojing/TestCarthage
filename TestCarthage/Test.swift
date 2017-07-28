8015a0cb602d6507cb6d0db5a7eb43b04e1de37e
JingZhao <zhou.jing@rea-group.com>
2017-07-28 10:52:24 +0800

    init framework

---
+++ TestCarthage/Test.swift
@@ -0,0 +1,5 @@
+public class Test: NSObject {
+    public func printTest() {
+        print("test")
+    }
+}
---
+++ TestCarthage/TestCarthage.h
@@ -0,0 +1,19 @@
+//
+//  TestCarthage.h
+//  TestCarthage
+//
+//  Created by JingZhao on 28/07/2017.
+//  Copyright © 2017 JingZhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestCarthage.
+FOUNDATION_EXPORT double TestCarthageVersionNumber;
+
+//! Project version string for TestCarthage.
+FOUNDATION_EXPORT const unsigned char TestCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestCarthage/PublicHeader.h>
+
+
